97a72aceb1c4145002fbc641ef512293e1cfefa9
therealsamyak <therealsamyak@gmail.com>
2024-10-26 15:09:30 -0700

    added basic class implementation * init for Node * init for Problem

--- puzzle.py
+++ puzzle.py
@@ -6,6 +6,15 @@ def print_puzzle(puzzle):
     print()
 
 
+class Node:
+    def __init__(self, parent: 'Node', state: list, score_func: callable) -> None:
+        self.parent = parent
+        self.state = state
+        self.score = score_func(state)
+
+
 class Problem:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cutoff: int, initial_state: list) -> None:
+        self.cutoff = cutoff
+        self.initial_state = Node(initial_state)
+        self.final_state = [(i + 1) % (cutoff**2) for i in range(cutoff**2)]
