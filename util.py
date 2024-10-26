ea4a37244e42aa9c051beef61f38cb60f3ce5ff4
therealsamyak <therealsamyak@gmail.com>
2024-10-26 15:47:53 -0700

    added structure for other util functions

--- util.py
+++ util.py
@@ -3,3 +3,10 @@ from puzzle import *
 def uniform_cost_heuristic(state: 'Node' = None):
     return 0
 
+def misplaced_tile(state: 'Node'):
+    # implement later
+    pass
+
+def euclidian_distance(state: 'Node'):
+    # implement later
+    pass
