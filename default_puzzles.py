d694bee29cf85f9acd9aa632e011fa2b2bc005eb
therealsamyak <therealsamyak@gmail.com>
2024-10-26 21:54:43 -0700

    added default puzzles

---
+++ default_puzzles.py
@@ -0,0 +1,11 @@
+trivial_puzzle = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+very_easy_puzzle = [1, 2, 3, 4, 5, 6, 7, 0, 8]
+
+easy_puzzle = [1, 2, 0, 4, 5, 3, 7, 8, 6]
+
+doable_puzzle = [0, 1, 2, 4, 5, 3, 7, 8, 6]
+
+oh_boy_puzzle = [8, 7, 1, 6, 0, 2, 5, 4, 3]
+
+impossible_puzzle = [1, 2, 3, 4, 5, 6, 8, 7, 0]
